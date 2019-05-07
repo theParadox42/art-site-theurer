bde217b7884524fda9317453703ef130df051912
Paradox Programming <nathaniel.fargo@gmail.com>
2019-05-07 22:40:44 +0000

    stuff

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
+// Dependents
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+
+// App setup
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+
+// Routes
 app.get("/", function(req, res){
     res.render("home");
 });
 app.get("/about", function(req, res){
-    res.render("about")
+    res.render("about");
+});
+app.get("*", function(req, res){
+    res.render("404");
 });
 
 
+// Listen
 app.listen(process.env.PORT || 8000, process.env.IP, function(){
     console.log("server started");
 });
